5759b84301ed65e43d80b16a450e27482f9ebc0a
buterbrot359 <batowka359@gmail.com>
2026-03-14 19:07:15 +0500

    init: frontend repo

---
+++ frontend/src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Providers } from './providers'
+import './styles/index.css'
+
+export function App() {
+  return (
+    <Providers>
+      <div>arrange-it</div>
+    </Providers>
+  )
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './index.css'
-import App from './App.tsx'
-
-const queryClient = new QueryClient()
+import { App } from '@/app'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 )
